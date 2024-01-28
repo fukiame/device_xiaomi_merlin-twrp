376570d69dc4a3d8e890519c86cc94685632e3c9
TillYourTearGoes <tillyourteargoes@proton.me>
2024-01-28 16:45:56 +0700

    selene: full_base_telephony -> base
    also move device ahead of common makefile inherit
    
    Signed-off-by: TillYourTearGoes <tillyourteargoes@proton.me>

--- twrp_selene.mk
+++ twrp_selene.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Inherit from selene device
 $(call inherit-product, device/xiaomi/selene/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := selene
 PRODUCT_NAME := twrp_selene
