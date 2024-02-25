5327d1721266405c241bc6984d732636e6cf11d6
Hiura Mihate <tillyourteargoes@murena.io>
2024-02-25 18:11:28 +0700

    selene: vendorsetup: ofox: setup flashlight
    Signed-off-by: Hiura Mihate <tillyourteargoes@murena.io>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -73,7 +73,7 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
     export OF_HIDE_NOTCH=1
     export OF_CLOCK_POS=1 # left and right clock positions available
     export OF_USE_GREEN_LED=0
-    #export OF_FL_PATH1="/tmp/flashlight"
+    export OF_FL_PATH1="/sys/devices/platform/flashlights_led191/leds/torch-light0"
 
     # Other OrangeFox configs
     export OF_ENABLE_LPTOOLS=1
@@ -85,7 +85,6 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
     export FOX_USE_NANO_EDITOR=1
 
     export FOX_DELETE_MAGISK_ADDON=1
-    export OF_FLASHLIGHT_ENABLE=0
     export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
   fi
 fi
